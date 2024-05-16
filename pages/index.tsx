a1692b83c41ce94d376f7064cdce83ec84749870
Paige Williams <paige.williams@aclima.io>
2024-05-16 15:00:36 -0700

    fetch bikeshare data with useSWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
-import Head from "next/head";
-import { Map } from "@/components/map";
+import Head from 'next/head';
+import { Map } from '@/components/map';
+import { useFetchBikeShare } from '@/hooks/useFetchBikeShare';
 
 export default function Home() {
+  const { data } = useFetchBikeShare();
+  console.log('data', data);
   return (
     <>
       <Head>
         <title>Map Prototype Example</title>
-        <meta name="description" content="Example for making geospatial prototypes" />
+        <meta
+          name="description"
+          content="Example for making geospatial prototypes"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main style={{width: "100%", height: "100vh"}}>
+      <main style={{ width: '100%', height: '100vh' }}>
         <Map />
       </main>
     </>
