d178868055ee2fc38428de2397812ed5fbc4a184
Paige Williams <paige.williams@aclima.io>
2024-05-16 15:41:14 -0700

    add bikeshare data to map
    add types for bikeshare data

--- components/map.tsx
+++ components/map.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Map as ReactGlMap, NavigationControl, useControl} from 'react-map-gl';
-import {GeoJsonLayer, ArcLayer} from 'deck.gl';
-import {MapboxOverlay as DeckOverlay} from '@deck.gl/mapbox';
+import { Map as ReactGlMap, NavigationControl, useControl } from 'react-map-gl';
+import { ScatterplotLayer } from 'deck.gl';
+import { MapboxOverlay as DeckOverlay } from '@deck.gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 // this is modified from this deckgl example, just to get a map on the page: https://github.com/visgl/deck.gl/blob/9.0-release/examples/get-started/react/mapbox/app.jsx
@@ -11,19 +11,17 @@ const AIR_PORTS =
   'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
 
 const INITIAL_VIEW_STATE = {
-  latitude: 51.47,
-  longitude: 0.45,
-  zoom: 4,
-  bearing: 0,
-  pitch: 30
+  longitude: -122.4,
+  latitude: 37.74,
+  zoom: 11,
 };
 
 const MAP_STYLE = 'mapbox://styles/mapbox/light-v9';
-const DeckGLOverlay = (props) => {
+const DeckGLOverlay = props => {
   const overlay = useControl(() => new DeckOverlay(props));
   overlay.setProps(props);
   return null;
-}
+};
 
 const onClick = info => {
   if (info.object) {
@@ -32,43 +30,36 @@ const onClick = info => {
   }
 };
 
-const layers = [
-  new GeoJsonLayer({
-    id: 'airports',
-    data: AIR_PORTS,
-    // Styles
-    filled: true,
-    pointRadiusMinPixels: 2,
-    pointRadiusScale: 2000,
-    getPointRadius: f => 11 - f.properties.scalerank,
-    getFillColor: [200, 0, 80, 180],
-    // Interactive props
-    pickable: true,
-    autoHighlight: true,
-    onClick
-  }),
-  new ArcLayer({
-    id: 'arcs',
-    data: AIR_PORTS,
-    dataTransform: (d: any) => d.features.filter(f => f.properties.scalerank < 4),
-    // Styles
-    getSourcePosition: f => [-0.4531566, 51.4709959], // London
-    getTargetPosition: f => f.geometry.coordinates,
-    getSourceColor: [0, 128, 200],
-    getTargetColor: [200, 0, 80],
-    getWidth: 1
-  })
-];
+type MapProps = {
+  bikeShareData: BikeShareFeature[];
+};
 
-export const Map = () => {
+export const Map: React.FC<MapProps> = ({ bikeShareData }) => {
+  const layers = [
+    new ScatterplotLayer({
+      id: 'bikeshares',
+      data: bikeShareData,
+      // Styles
+      getPosition: ({ geometry }) => [geometry.x, geometry.y],
+      radiusMinPixels: 3,
+      radiusMaxPixels: 15,
+      getFillColor: [255, 140, 0],
+      getLineColor: [0, 0, 0],
+      getLineWidth: 10,
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick,
+    }),
+  ];
+
   return (
     <ReactGlMap
       initialViewState={INITIAL_VIEW_STATE}
       mapStyle={MAP_STYLE}
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-    >
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}>
       <DeckGLOverlay layers={layers} />
       <NavigationControl position="bottom-right" />
     </ReactGlMap>
   );
-}
+};
--- hooks/useFetchBikeShare.ts
+++ hooks/useFetchBikeShare.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import { fetcher } from '@/utils/fetch';
 
 export const useFetchBikeShare = () => {
-  return useSWR(
+  return useSWR<BikeShareResponse>(
     'https://geo.dot.gov/server/rest/services/Hosted/Bikeshare_Scooter_Systems_DS/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json',
     fetcher,
   );
