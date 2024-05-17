8f450bf0d4d5863b3856f8e7496c67bec3a37c2e
Paige Williams <paige.williams@aclima.io>
2024-05-17 16:55:29 -0700

    add mantine for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
+import '@mantine/core/styles.css';
 import Head from 'next/head';
+import { AppShell, MantineProvider } from '@mantine/core';
+
 import { Map } from '@/components/map';
 import { Header } from '@/components/header';
-import theme, { SourceSans } from '@/utils/theme';
+import { SourceSans } from '@/utils/theme';
 import { useFetchBikeShare } from '@/hooks/useFetchBikeShare';
 import { useFetchAmtrakRoutes } from '@/hooks/useFetchAmtrakRoutes';
+import { theme } from '../theme';
 
 export default function Home() {
   const { data: bikeShareData } = useFetchBikeShare();
   const { data: amtrakData } = useFetchAmtrakRoutes();
 
   return (
-    <>
+    <MantineProvider theme={theme}>
       <Head>
         <title>Map Prototype Example</title>
         <meta
@@ -20,11 +24,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <AppShell></AppShell>
       <main className={SourceSans.className}>
         <style jsx global>{`
           body {
             margin: 0;
-            color: ${theme.colors.textGray};
+            color: ${theme.colors.gray[9]};
           }
         `}</style>
         <Header />
@@ -39,6 +44,6 @@ export default function Home() {
           amtrakData={amtrakData ?? { type: 'FeatureCollection', features: [] }}
         />
       </main>
-    </>
+    </MantineProvider>
   );
 }
--- utils/theme.tsx
+++ utils/theme.tsx
@@ -1,5 +1,4 @@
 import { Source_Sans_3 } from 'next/font/google';
-import { text } from 'stream/consumers';
 
 export const SourceSans = Source_Sans_3({
   weight: ['400', '600', '700'],
@@ -8,13 +7,13 @@ export const SourceSans = Source_Sans_3({
 });
 
 const prototypeStylingTheme = {
-    colors: {
-        backgroundGray: '#F5F5F5',
-        textGray: '#222222', 
-    },
-    breakpoints: {
-        mobile: '600px',
-    }
-}
+  colors: {
+    backgroundGray: '#F5F5F5', // gray0
+    textGray: '#222222', // gray9
+  },
+  breakpoints: {
+    mobile: '600px',
+  },
+};
 
 export default prototypeStylingTheme;
