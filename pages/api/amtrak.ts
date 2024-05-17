067d8fe2af6f675af97561f950b60f313836b417
Paige Williams <paige.williams@aclima.io>
2024-05-17 09:48:45 -0700

    add comment for amtrak data source

--- pages/api/amtrak.ts
+++ pages/api/amtrak.ts
@@ -2,6 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import amtrakData from '@/data/Amtrak_Routes.json';
 
+// amtrak routes data source:
+// https://geodata.bts.gov/datasets/usdot::amtrak-routes/about
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(amtrakData);
 }
