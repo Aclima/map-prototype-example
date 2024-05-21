edd7934980334724f36a922dd97a01a440efeff2
Paige Williams <paige.williams@aclima.io>
2024-05-21 13:05:50 -0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import '../global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId={process.env.GA_TRACKING_ID} />
+    </>
+  );
 }
